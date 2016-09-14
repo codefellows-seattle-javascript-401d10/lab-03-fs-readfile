ed342d980e0247770f223b93ea28f02a4bc0692d
Logan Rogers <logan.s.rogers@biola.edu>
2016-09-14 14:03:57 -0700

    started on testing

---
+++ test/index-test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const assert = require('assert');
+
+describe('Testing readFile module', function(){
+  it('Should return 8 bytes in hex from each file', function(){
+    assert.equal();
+  });
+});
