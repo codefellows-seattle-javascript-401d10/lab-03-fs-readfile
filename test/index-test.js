55d292056e92c8713dec1ae510375139d95bc1c6
Logan Rogers <logan.s.rogers@biola.edu>
2016-09-14 16:23:03 -0700

    test passes

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,13 @@
 'use strict';
 
+const readFileHelper = require('../lib/index.js');
 const assert = require('assert');
 
-describe('Testing readFile module', function(){
-  it('Should return 8 bytes in hex from each file', function(){
-    assert.equal();
+describe('Testing readFileHelper module', function(){
+  it('Should return 8 bytes in hex from each file', function(done){
+    readFileHelper(function(text){
+      assert.equal(text, '6c756c7761743a33');
+      done();
+    });
   });
 });
