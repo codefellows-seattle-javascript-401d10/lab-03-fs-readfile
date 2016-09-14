f620129e39518162c1ac6a1c59c205302c7fa4b1
kaylynyuh <yuhkay4@gmail.com>
2016-09-14 14:36:25 -0700

    re-structured read file function return to results in order

--- test/fs-test.js
+++ test/fs-test.js
@@ -2,3 +2,9 @@
 
 const readFilesFn = require('../lib/fs.js');
 const assert = require('assert');
+
+describe('testing fs module', function() {
+  it('should read three files and output 8 bytes of data per file', function() {
+    
+  });
+});
