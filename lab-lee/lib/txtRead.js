0673d93a01c4c4cbf1a9bda353567cfe7e6822ef
Lee Broxson <broxsonl@gmail.com>
2016-09-14 21:59:20 -0700

    Cleaned up extraneous code.

--- lab-lee/lib/txtRead.js
+++ lab-lee/lib/txtRead.js
@@ -5,8 +5,6 @@ const fs = require('fs');
 var hexArray = [];
 
 module.exports = function(callback) {
-  // fs.readdir(`${__dirname}/../data`, function(err, files) {
-  // });
   fs.readFile(`${__dirname}/../data/1.txt`, function(err, data) {
     hexArray.push(data.toString('hex', 0, 8));
     fs.readFile(`${__dirname}/../data/2.txt`, function(err, data) {
@@ -18,6 +16,3 @@ module.exports = function(callback) {
     });
   });
 };
-
-// fs.readdir(`${__dirname}/../data`, function(err, files) {
-// });
