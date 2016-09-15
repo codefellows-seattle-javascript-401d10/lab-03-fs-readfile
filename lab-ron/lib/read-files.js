9974d31bf3bb076e42e03a3ab50afb637f091b4f
Rubiksron <rubiksron@gmail.com>
2016-09-14 20:51:02 -0700

    was able to print the 3 files to CL, having pushed to an array, not done.

---
+++ lab-ron/lib/read-files.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const fs = require('fs');
+
+module.exports = exports = {};
+var array = [];
+module.exports = function(callback){
+  fs.readFile(`${__dirname}/../data/one.txt`, function(err, data){
+    if(err) throw err;
+    array.push(data.toString('hex', 0, 8));
+    fs.readFile(`${__dirname}/../data/two.txt`, function(err, data){
+      if(err) throw err;
+      array.push(data.toString('hex', 0, 8));
+      fs.readFile(`${__dirname}/../data/three.txt`, function(err, data){ //all NODE API calls take err, data, in that order.
+        if(err) throw err;
+        array.push(data.toString('hex', 0, 8));
+        console.log('data from line 23', array);
+      });
+    });
+  });
+};
+
+
+
+// const assert = require('assert');
+// const readFileHelper = require('../lib/read-file-helper.js');
+//
+// describe('testing readFileHelper', function(){
+//   it('should return the right string', function(done){
+//     readFileHelper(function(test){
+//     assert.equal(text, 'hello world');
+//       done();
+//     });
+//   });
+// });
