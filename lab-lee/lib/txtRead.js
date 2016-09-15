01f5be98227e980af36c59dbd9c128a56913848f
Lee Broxson <broxsonl@gmail.com>
2016-09-14 22:18:41 -0700

    Added errors throws.

--- lab-lee/lib/txtRead.js
+++ lab-lee/lib/txtRead.js
@@ -6,10 +6,13 @@ var hexArray = [];
 
 module.exports = function(callback) {
   fs.readFile(`${__dirname}/../data/1.txt`, function(err, data) {
+    if(err) throw new Error('1.txt not read right.');
     hexArray.push(data.toString('hex', 0, 8));
     fs.readFile(`${__dirname}/../data/2.txt`, function(err, data) {
+      if(err) throw new Error('2.txt not read right.');
       hexArray.push(data.toString('hex', 0, 8));
       fs.readFile(`${__dirname}/../data/3.txt`, function(err, data) {
+        if(err) throw new Error('3.txt not read right.');
         hexArray.push(data.toString('hex', 0, 8));
         callback(hexArray);
       });
