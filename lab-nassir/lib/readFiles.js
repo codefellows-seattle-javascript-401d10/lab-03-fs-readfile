7ecf51c52477593cdfb74775b48ed55da858274e
Nassir Isaf <njisaf@gmail.com>
2016-09-14 17:06:13 -0700

    Console.log cleanups

--- lab-nassir/lib/readFiles.js
+++ lab-nassir/lib/readFiles.js
@@ -9,16 +9,12 @@ const fs = require('fs');
 
 module.exports = function readDataFiles(callback) {
   var resultArray = [];
-  // process.nextTick(orderFiles);
   fs.readFile(`${__dirname}/../data/one.txt`, function(err, data){
     resultArray.push(data.toString('hex', 0, 7));
-    // console.log('one', resultArray);
     fs.readFile(`${__dirname}/../data/two.txt`, function(err, data){
       resultArray.push(data.toString('hex', 0, 7));
-      // console.log('two', resultArray);
       fs.readFile(`${__dirname}/../data/three.txt`, function(err, data){
         resultArray.push(data.toString('hex', 0, 7));
-        // console.log('three', resultArray);
         callback(resultArray);
       });
     });
