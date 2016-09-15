2b0e31d5e8106f27c20c90db42f49ed22ea591e6
Nassir Isaf <njisaf@gmail.com>
2016-09-14 17:00:39 -0700

    Testing function complete

--- lab-nassir/test/readFiles-test.js
+++ lab-nassir/test/readFiles-test.js
@@ -2,8 +2,30 @@
 
 // node modules
 // const fs = require('fs');
-const readDataFiles = require('./lib/readFiles');
+const readDataFiles = require('../lib/readFiles');
+const assert = require('assert');
 // npm modules
 // app modules
 // module constants
 // module logic
+
+describe('readDataFiles test', function(){
+  it('should discover a particular string in index [0] of text', function(done){
+    readDataFiles(function(text){
+      assert.equal(text[0], '4c6f72656d2069');
+      done();
+    });
+  });
+  it('should discover a particular string in index [1] of text', function(done){
+    readDataFiles(function(text){
+      assert.equal(text[1], '4261636f6e2069');
+      done();
+    });
+  });
+  it('should discover a particular string in index [2] of text', function(done){
+    readDataFiles(function(text){
+      assert.equal(text[2], '43757063616b65');
+      done();
+    });
+  });
+});
