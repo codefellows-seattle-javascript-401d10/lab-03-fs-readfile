10d7b0cc56e4b7306ec09f4d8f4490698b39dc87
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-09-14 23:02:00 -0700

    got the function, having trouble with the test
    my console.logs are coming up with what I want in the function
    the tests aren't spitting out anything useful at the moment
    I will rework them in the morning with some help

--- lab-elizabeth/lib/data-files-read.js
+++ lab-elizabeth/lib/data-files-read.js
@@ -1,20 +1,24 @@
 'use strict';
 
 const fs = require('fs');
-const testBuf = new Buffer(24);
 
 module.exports = function(callback){
+  const testBuf = new Buffer(24);
+  const loadOrderArray = [];
   fs.readFile(`${__dirname}/../data/one.txt`, function(err, buf1){
     if(err) throw err;
+    loadOrderArray.push('one.txt');
     buf1.copy(testBuf, 0, 0, 8);
     fs.readFile(`${__dirname}/../data/two.txt`, function(err, buf2){
       if(err) throw err;
+      loadOrderArray.push('two.txt');
       buf2.copy(testBuf, 9, 0, 8);
       fs.readFile(`${__dirname}/../data/three.txt`, function(err, buf3){
         if(err) throw err;
         buf3.copy(testBuf, 17, 0, 8);
-        var data = testBuf.toString('hex');
-        callback(data);
+        loadOrderArray.push('three.txt');
+        var hexText = testBuf.toString('hex');
+        callback(loadOrderArray, hexText);
       });
     });
   });
