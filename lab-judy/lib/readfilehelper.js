3401964361b454462b780b973ae3ed2802f25f0d
JudyVue <judyvue87@gmail.com>
2016-09-14 18:15:35 -0700

    validly passing tests for each function and got two arrays to match

--- lab-judy/lib/readfilehelper.js
+++ lab-judy/lib/readfilehelper.js
@@ -8,24 +8,29 @@ exports.readOneText = readOneText;
 exports.readTwoText = readTwoText;
 exports.readThreeText = readThreeText;
 
+exports.callbackHexStringsArray = [];
+
 function readOneText(callback){
-  fs.readFile('./data/one.txt', (err, data) => {
+  fs.readFile(`${__dirname}/../data/one.txt`, (err, data) => {
     if (err) throw err;
     callback(data.toString('utf8', 0, 8));
+    exports.callbackHexStringsArray.push(data.toString('utf8', 0, 8));
   });
 }
 
 
 function readTwoText(callback) {
-  fs.readFile('./data/two.txt', (err, data) => {
+  fs.readFile(`${__dirname}/../data/two.txt`, (err, data) => {
     if (err) throw err;
     callback(data.toString('utf8', 0, 8));
+    exports.callbackHexStringsArray.push(data.toString('utf8', 0, 8));
   });
 }
 
 function readThreeText(callback){
-  fs.readFile('./data/three.txt', (err, data) => {
+  fs.readFile(`${__dirname}/../data/three.txt`, (err, data) => {
     if (err) throw err;
+    exports.callbackHexStringsArray.push(data.toString('utf8', 0, 8));
     callback(data.toString('utf8', 0, 8));
   });
 }
--- lab-judy/test/index-test.js
+++ lab-judy/test/index-test.js
@@ -3,42 +3,47 @@
 
 const readfilehelper = require('../lib/readfilehelper');
 const assert = require('assert');
+const callBackArray = readfilehelper.callbackHexStringsArray;
 
 describe('testing index', function(){
-  var stringArray = [];
+  var indexTestStringArray = [];
   //testing one.text
   describe('testing #readOneText()', function(){
-    it ('should return 12345678', function(done){
-      let result = readfilehelper.readOneText(function(hexString){
-        console.log(hexString);
-        stringArray.push(hexString);
+    it ('should return 11111111', function(done){
+      readfilehelper.readOneText(function(hexString){
+        console.log(hexString, 'at line 13');
+        indexTestStringArray.push(hexString);
+        assert.equal(hexString, '11111111', 'did not get expected result');
         done();
-        //test goes here
-        assert.equal(result, 12345678, 'did not get expected result 12345678');
       });
     });
   });
   //testing two.text
   describe('testing #readTwoText()', function(){
-    it('should return 12345678', function(done){
-      let result = readfilehelper.readTwoText(function(hexString){
+    it('should return 22222222', function(done){
+      readfilehelper.readTwoText(function(hexString){
         console.log(hexString);
-        stringArray.push(hexString);
+        indexTestStringArray.push(hexString);
+        assert.equal(hexString, '22222222', 'did not get expected result 22222222');
         done();
-        //test goes here
-        assert.equal(result, 12345678, 'did not get expected result 12345678');
       });
     });
   });
   //testing three.text
   describe('testing #readThreeText()', function(){
     it('should return 1234567', function(done){
-      let result = readfilehelper.readThreeText(function(hexString){
+      readfilehelper.readThreeText(function(hexString){
         console.log(hexString);
-        stringArray.push(hexString);
+        indexTestStringArray.push(hexString);
+        assert.equal(hexString, 1234567, 'did not get expected result 1234567');
+
+        //testing if two arrays match
+        describe('testing to see if data in indexTestStringArray matches the callBackArray that was populated in readfilehelper.js', function(){
+          it(',should assert if the array elements match ', function(){
+            assert.equal(indexTestStringArray.join, callBackArray.join, 'arrays dont match');
+          });
+        });
         done();
-        //test goes here
-        assert.equal(result, 1234567, 'did not get expected result 1234567');
       });
     });
   });
