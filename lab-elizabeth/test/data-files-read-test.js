14166f9b8b7e0994fd260196f949e5018c121dd2
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-09-15 08:53:16 -0700

    forgot my eslint file

--- lab-elizabeth/test/data-files-read-test.js
+++ lab-elizabeth/test/data-files-read-test.js
@@ -5,14 +5,14 @@ const assert = require('assert');
 
 describe('determining load order and data is correct', function(){
   it('verifies load-order', function(done){
-    dataFilesRead(function(array, text){
+    dataFilesRead(function(array){
       assert.equal(array, ['one.txt', 'two.txt', 'three.txt'], 'expected array of one, two, three');
       done();
     });
   });
   it('checks the data', function(done){
     dataFilesRead(function(array, text){
-      assert.equal(text, '42726f776e20636104426c656768626c654c69636b20706c', 'expected hex output for one-three.txt');
+      assert.equal(text, '42726f776e20636104426c656768626c654c69636b20706c', 'expected hex output for one, two, three.txt');
       done();
     });
   });
