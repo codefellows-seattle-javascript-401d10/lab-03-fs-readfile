5a7fc7a1b5220583e1bbd5598a5c77c3b92753b4
Lee Broxson <broxsonl@gmail.com>
2016-09-14 18:46:18 -0700

    some cleanup. Started trying to work on having no nesteds.

--- lab-lee/lib/txtRead.js
+++ lab-lee/lib/txtRead.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 var indexData = [];
 
 module.exports = function(callback) {
+  fs.readdir(`${__dirname}/../data`, function(err, files) {
+    console.log(files);
+  });
   fs.readFile(`${__dirname}/../data/1.txt`, function(err, data) {
     indexData.push(data.toString('hex', 0, 8));
     fs.readFile(`${__dirname}/../data/2.txt`, function(err, data) {
@@ -16,3 +19,7 @@ module.exports = function(callback) {
     });
   });
 };
+
+fs.readdir(`${__dirname}/../data`, function(err, files) {
+  console.log(files);
+});
