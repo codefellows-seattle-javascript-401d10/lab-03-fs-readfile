1c74bfae29d0c431aabb16cfe1e2514c235ecd51
Peter Kim <peterkim23@gmail.com>
2016-09-18 21:56:43 -0700

    fixed false positive result

--- lab-peter/lib/readfile.js
+++ lab-peter/lib/readfile.js
@@ -8,15 +8,15 @@ var myArray = [];
 
 module.exports = function(callback) {
   fs.readFile(`${__dirname}/../data/one.txt`, function(err, data) {
-    if (err) throw new Error('text one is wrong');
+    if (err) throw err('text one is wrong');
     myArray.push(data.toString('hex', 0, 8));
-      fs.readFile(`${__dirname}/../data/two.txt`, function(err, data) {
-        if (err) throw new Error('text two is wrong');
+    fs.readFile(`${__dirname}/../data/two.txt`, function(err, data) {
+      if (err) throw err('text two is wrong');
+      myArray.push(data.toString('hex', 0, 8));
+      fs.readFile(`${__dirname}/../data/three.txt`, function(err, data) {
+        if (err) throw err('text three is wrong');
         myArray.push(data.toString('hex', 0, 8));
-          fs.readFile(`${__dirname}/../data/three.txt`, function(err, data) {
-            if (err) throw new Error('text three is wrong');
-            myArray.push(data.toString('hex', 0, 8));
-            callback(myArray);
+        callback(myArray);
       });
     });
   });
--- lab-peter/test/fs-readfile-test.js
+++ lab-peter/test/fs-readfile-test.js
@@ -7,7 +7,7 @@ describe('testing index', function() {
   describe('#readFile()', function() {
     it ('should read one.txt two.txt three.txt', function(done) {
       readFile(function(myArray) {
-        assert.equal(myArray.join(),['52616e646f6d2074','536f6d6520616464','46696e616c6c7920']); //text is your myArray
+        assert.equal(myArray.join(),'52616e646f6d2074,536f6d6520616464,46696e616c6c7920');
         done();
       });
     });
