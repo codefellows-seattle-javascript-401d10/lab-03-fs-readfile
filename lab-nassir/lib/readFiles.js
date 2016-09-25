9f68fb18b58b5e1aae00b385fe687e144129fa13
Nassir Isaf <njisaf@gmail.com>
2016-09-25 15:35:29 -0700

    Refactor complete

--- lab-nassir/lib/readFiles.js
+++ lab-nassir/lib/readFiles.js
@@ -10,54 +10,13 @@ const fs = require('fs');
 module.exports = function readDataFiles(callback) {
   var resultArray = [];
   fs.readFile(`${__dirname}/../data/one.txt`, function(err, data){
-    resultArray.push(data.toString('hex', 0, 7));
+    resultArray.push(data.toString('hex', 0, 8));
     fs.readFile(`${__dirname}/../data/two.txt`, function(err, data){
-      resultArray.push(data.toString('hex', 0, 7));
+      resultArray.push(data.toString('hex', 0, 8));
       fs.readFile(`${__dirname}/../data/three.txt`, function(err, data){
-        resultArray.push(data.toString('hex', 0, 7));
+        resultArray.push(data.toString('hex', 0, 8));
         callback(resultArray);
       });
     });
   });
 };
-
-//new stuff from Duncan's solution.
-
-// const readFiles = module.exports = function(pathArray, callback){
-//   var result = [];
-//   fs.readFile(pathArray[0], function(err, data){
-//     if (err) return callback(err); //return will end the function. This will trigger if pathArray[0] contains no valid data.
-//     result.push(etc);
-//     //repeat two times
-//     callback(null, result);
-//   });
-// };
-//
-// readFiles([`${__dirname}/dont-exist.txt`], function(err, data){
-//   if (err) {
-//     console.error('error found here');
-//     console.error(err);
-//   }
-// });
-
-// old attempt at extracred, failure
-
-// module.exports = function readDataFiles(callback) {
-//   var resultArray = [];
-//   fs.readdir(`${__dirname}/../data/`, function(err, files) {
-//     console.log(files);
-//     for (var i = 0; i <= files.length; i++) {
-//       if (resultArray.length === files.length) {
-//         callback(resultArray);
-//         console.log('finally');
-//       } else if (resultArray.length < files.length) {
-//         fs.readFile(`${__dirname}/../data/${files[i]}`, function(err, data){
-//           console.log(data);
-//           console.log(data.toString('hex', 0, 7));
-//           resultArray.push(data.toString('hex', 0, 7));
-//           console.log(resultArray);
-//         });
-//       }
-//     }
-//   });
-// };
--- lab-nassir/test/readFiles-test.js
+++ lab-nassir/test/readFiles-test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // node modules
-// const fs = require('fs');
 const readDataFiles = require('../lib/readfiles');
 const assert = require('assert');
 // npm modules
@@ -12,42 +11,10 @@ const assert = require('assert');
 describe('readDataFiles test', function(){
   it('Items in resultsArray should match to their expected index positions', function(done){
     readDataFiles(function(text){
-      assert.equal(text[0], '4c6f72656d2069', 'resultArray at index[0] is not matching with test; item is out of position or incorrect');
-      assert.equal(text[1], '4261636f6e2069', 'resultArray at index[1] is not matching with test; item is out of position or incorrect');
-      assert.equal(text[2], '43757063616b65', 'resultArray at index[2] is not matching with test; item is out of position or incorrect');
+      assert.equal(text[0], '4c6f72656d206970', 'resultArray at index[0] is not matching with test; item is out of position or incorrect');
+      assert.equal(text[1], '4261636f6e206970', 'resultArray at index[1] is not matching with test; item is out of position or incorrect');
+      assert.equal(text[2], '43757063616b6520', 'resultArray at index[2] is not matching with test; item is out of position or incorrect');
       done();
-    });
-  });
-});
-
-//chai demo from duncan
-
-const expect = require('chai').expect;
-const readFiles = require('etc');
-
-describe('describe all tests in block', function(){
-  describe('first test, subsequent tests get own describe i think', function(){
-    it('should return error', function(){
-      readFiles([`${__dirname}/dont-exist.txt`], function(err, data){
-        expect(!!err).to.equal(true);
-      });
-    });
-  });
-  describe('good paths', function(HERE){
-    before((done) => { // this lexical, fatarrow, or arrow function inherits the context of the parent function. so if we use this., it'll inherit the this of the previous function. I've put HERE into the inherited function
-      this.paths = [
-        'path1',
-        'path2',
-        'path3',
-      ];
-      done();
-    });
-    it('return an error', (done) => { //here again, we're inheriting the context of the HERE function. The this. on the next line also does that.
-      readFiles(this.paths, function(err, data){
-        expect(err).to.be(null);
-        console.log(err);
-        done();
-      });
     });
   });
 });
